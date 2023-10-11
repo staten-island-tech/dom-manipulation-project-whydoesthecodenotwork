fe28ec5fbc318b81c0239b9f9a92c722b1db3ae7
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2023-10-11 00:37:59 -0400

    use more website

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const DOMSelectors = {
+let DOMSelectors = {
     body: document.querySelector("body"),
     form: document.querySelector("body > form"),
     spam: document.querySelector("body > #spam"),
+    /* > means it only selects direct children instead of all the children */
     counter: document.getElementById("counter"),
     /* inspect element -> right click element -> copy -> copy JS path 
 	also spam uses a # id selector; class selectors are .*/
+    /* querySelectorAll is a node list, querySelector is the first element that matches selection 
+    getElementsByClassName is an HTMLCollection and does not support forEach. how rude*/
+    input: document.querySelector("body > form > input[name='input']"),
 };
 
-input = DOMSelectors["form"].children[0];
-
 function go() {
-    if (input.value !== "") {
-        append(input.value);
+    const input = DOMSelectors["input"].value;
+    if (input !== "") {
+        append(input);
     }
 }
 
@@ -26,15 +29,14 @@ function append(obj) {
 }
 
 function clearInput() {
-    input.value = "";
+    DOMSelectors["input"].value = "";
 }
 
 var counter = 0;
 
 function remove() {
-    const targets = spam.querySelectorAll(".user");
-    /* querySelectorAll is a node list, querySelector is the first element that matches selection 
-	getElementsByClassName is an HTMLCollection and does not support forEach. how rude*/
+    targets = document.querySelectorAll("body > #spam .user");
+    /* this can't actually go into the DOMSelectors object because then the list won't update as the user adds more h3s */
     targets.forEach((element) => element.remove());
     counter += 1;
     DOMSelectors["counter"].innerHTML = "times cleared: " + counter;
