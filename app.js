1c66e8a91f4d0b088a54f165c82dac388420fbc3
whydoesthecodenotwork <lawrencel54@nycstudents.net>
2023-10-12 13:11:53 -0400

    use more website
    I gave myself a double period

--- app.js
+++ app.js
@@ -1,64 +1,72 @@
-let DOMSelectors = {
-    body: document.querySelector("body"),
-    form: document.querySelector("body > form"),
-    spam: document.querySelector("body > #spam"),
-    /* > means it only selects direct children instead of all the children */
-    counter: document.getElementById("counter"),
-    /* inspect element -> right click element -> copy -> copy JS path 
+const DOMSelectors = {
+	body: document.querySelector("body"),
+	form: document.querySelector("body > form"),
+	spam: document.querySelector("body > #spam"),
+	/* > means it only selects direct children instead of all the children */
+	counter: document.getElementById("counter"),
+	/* inspect element -> right click element -> copy -> copy JS path 
 	also spam uses a # id selector; class selectors are .*/
-    /* querySelectorAll is a node list, querySelector is the first element that matches selection 
+	/* querySelectorAll is a node list, querySelector is the first element that matches selection 
     getElementsByClassName is an HTMLCollection and does not support forEach. how rude*/
-    input: document.querySelectorAll(
-        "body > form label > input:not([type=submit])"
-    ),
+	input: document.querySelectorAll(
+		"body > form label > input:not([type=submit])"
+	),
+	template: document.querySelector("#templatecard"),
 };
 
 function go() {
-    const inputs = [];
-    DOMSelectors["input"].forEach(
-        (input) => (inputs[input.name] = input.value)
-    );
-    /* turn the list of inputs into just the names and values */
-    append(inputs);
-    clearInput();
+	const inputs = [];
+	DOMSelectors["input"].forEach(
+		(input) => (inputs[input.name] = input.value)
+	);
+	/* turn the list of inputs into just the names and values */
+	append(inputs);
+	clearInput();
 }
 
 function append(obj) {
-    const newCard = document.createElement("div");
-    const newName = document.createElement("h3");
-    const name = document.createTextNode(
-        "NAME: " + obj["firstName"] + " " + obj["lastName"]
-    );
-    newCard.className = "card";
-    newName.className = "name";
-    newName.appendChild(name);
-    newCard.appendChild(newName);
-    /* new name is container for the name text node. appending the text node directly doesn't add it as a separate child */
+	console.log(obj);
 
-    const newAge = document.createElement("h3");
-    const age = document.createTextNode("AGE: " + obj["age"]);
-    newAge.append(age);
-    newCard.append(newAge);
+	const newCard =
+		DOMSelectors["template"].content.cloneNode(true).children[0];
+	console.log(newCard);
 
-    const deleteButton = document.createElement("button");
+	const properties = {
+		name: newCard.querySelector(".name"),
+		age: newCard.querySelector(".age"),
+		avatar: newCard.querySelector(".avatar"),
+		institution: newCard.querySelector(".institution"),
+		text: newCard.querySelector(".text"),
+	};
+	console.log(properties);
 
-    deleteButton.setAttribute("onclick", "remove(this);");
-    deleteButton.innerText = "delete";
+	properties["name"].innerText = "name: " + obj["name"];
 
-    newCard.append(deleteButton);
+	properties["age"].innerText = "age: " + obj["age"];
 
-    DOMSelectors["spam"].appendChild(newCard);
-    /* DOMSelectors['body'].children["spam"] would also work */
+	properties["avatar"].style.background = obj["color"];
+
+	obj["institution"]
+		? (properties["institution"].innerText = obj["institution"])
+		: properties["institution"].remove();
+
+	properties["text"].style["text-shadow"] = "1px 1px 2px " + obj["color"];
+	// this feels criminal
+
+	DOMSelectors["spam"].appendChild(newCard);
+	// /* DOMSelectors['body'].children["spam"] would also work */
 }
 
 function clearInput() {
-    DOMSelectors["input"].forEach((input) => (input.value = ""));
+	DOMSelectors["input"].forEach(
+		(input) => (input.value = input.defaultValue)
+	);
 }
 
 var counter = 0;
 
 function remove(obj) {
-    counter += 1;
-    DOMSelectors["counter"].innerHTML = "times cleared: " + counter;
-    obj.parentNode.remove();
+	counter += 1;
+	DOMSelectors["counter"].innerHTML = "cards deleted: " + counter;
+	obj.remove();
 }
