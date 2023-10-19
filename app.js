00fc0391e8e8f2a84fc69375d21b769b9fc1ce93
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2023-10-19 17:29:05 -0400

    Use more website
    template literals my beloved

--- app.js
+++ app.js
@@ -23,89 +23,50 @@ function go() {
 }
 
 function append(obj) {
-    console.log(obj);
-
-    /* 	const newCard =
-		DOMSelectors["template"].content.children[0].cloneNode(true);
-	// .content gets the document fragment of template
-	// children[0] is to get the card (index 0) from the document fragment
-	console.log(newCard);
-
-	const properties = {
-		name: newCard.querySelector(".name"),
-		issuedate: newCard.querySelector(".issuedate"),
-		avatar: newCard.querySelector(".avatar"),
-		institution: newCard.querySelector(".institution"),
-		text: newCard.querySelector(".text"),
-		barcode: newCard.querySelector("#barcode"),
-	};
-	// DOMSelectors object is full of document.blabla but we need newCard.blabla, so don't use DOMSelectors here
-	console.log(properties);
-
-	properties["name"].innerText = "name: " + obj["name"];
-
-	obj["issuedate"]
-		? (properties["issuedate"].innerText =
-				"issue date: " +
-				new Date(obj["issuedate"] + "T00:00:00").toLocaleDateString())
-		: properties["issuedate"].remove();
-
-	// if no issue date was provided, get rid of the issue date element
-
-	properties["avatar"].style.background = obj["color"];
-	properties["avatar"].alt = obj["name"];
-
-	obj["institution"]
-		? (properties["institution"].innerText = obj["institution"])
-		: properties["institution"].remove();
-
-	properties["text"].style["text-shadow"] = "1px 1px 2px " + obj["color"];
-	// mixing the [] and . feels criminal
-
-	obj["id"]
-		? JsBarcode(properties["barcode"], obj["id"], {
-				displayValue: false,
-				format: "code39",
-				width: 3,
-				height: 80,
-		  })
-		: properties["barcode"].remove(); */
+    // console.log(obj);
 
     DOMSelectors["spam"].insertAdjacentHTML(
         "beforeend",
-        `
-	<div class="card" onclick="remove(this);">
-		<h2 class="institution">${obj.institution}</h2>
-		<div class="cardcontent">
-			<img class="avatar" src="profile.png" alt="${obj.name}" />
-			<div class="text">
-				<h3 class="name">${obj.name}</h3>
-				<h3 class="issuedate">${new Date(
-                    obj["issuedate"] + "T00:00:00"
-                ).toLocaleDateString()}</h3>
-			</div>
-		</div>
-		<svg class="barcode"></svg>
-	</div>
-	`
+        `<div class="card" onclick="remove(this);">
+            <h2 class="institution">${obj.institution}</h2>
+            <div class="cardcontent">
+                <img class="avatar" src="profile.png" alt="${
+                    obj.name
+                }" style="background-color:${obj.color}"/>
+                <div class="text" style="text-shadow:1px 1px 2px ${
+                    obj["color"]
+                }">
+                    <h3 class="name">name: ${obj.name}</h3>
+                    <h3 class="issuedate">issue date: ${new Date(
+                        obj["issuedate"] + "T00:00:00"
+                    ).toLocaleDateString()}</h3>
+                </div>
+            </div>
+            <svg class="barcode"></svg>
+        </div>`
     );
-    newCard = DOMSelectors["spam"].lastElementChild;
+
+    newCard = DOMSelectors["spam"].lastElementChild; // get the card we just added
     const properties = {
         name: newCard.querySelector(".name"),
         issuedate: newCard.querySelector(".issuedate"),
-        avatar: newCard.querySelector(".avatar"),
         institution: newCard.querySelector(".institution"),
-        text: newCard.querySelector(".text"),
-        barcode: newCard.querySelector(".barcode"),
-    };
-    console.log(Object.keys(obj));
+        id: newCard.querySelector(".barcode"),
+    }; // then get its elements
+
     Object.keys(obj).forEach((key) => {
-        console.log(key);
         if (obj[key] == "") {
-            console.log("GOODBYE");
-            properties[key].remove();
+            properties[key].remove(); // if the user left the input field for that element blank, remove it.
         }
     });
+    if (properties.id) {
+        JsBarcode(properties.id, obj["id"], {
+            displayValue: false,
+            format: "code39",
+            width: 3,
+            height: 70,
+        });
+    }
     // DOMSelectors['body'].children["spam"] would get the spam object
 }
 
